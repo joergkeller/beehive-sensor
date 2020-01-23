368f4b16e6573b12e95cfc48f675ee5fbe707eab
jokel <joerg.p.keller@gmail.com>
2020-01-24 00:34:05 +0100

    recording sensor data in doc-storage (dynamo-db)

--- beehive-recorder/src/app.js
+++ beehive-recorder/src/app.js
@@ -1,21 +1,16 @@
 const axios = require('axios')
 
+const region = 'eu-central-1';
+const dbTable = 'SensorData';
 const url = 'https://api.thingspeak.com/update.json';
-const sampleMessage = {
-    api_key: "L4I0S01H3X70958G",
-    channel_id: 941478,
-    "field1": 0,
-    "field2": null,
-    "field3": null,
-    "field4": 1.2,
-    "field6": 98.1,
-    "field7": 3.8,
-    "field8": 1.7,
-    "status": "version 0, 3.8 V",
-    created_at: '2019-12-16T05:54:35.630735468Z'
-};
 
 const devices = [{
+    dev_id: 'cube-cell-1',
+    thingspeak: {
+        channel_id: 941478,
+        api_key: 'L4I0S01H3X70958G',
+    }
+},{
     dev_id: 'test-987',
     thingspeak: {
         channel_id: 941478,
@@ -39,12 +34,14 @@ const devices = [{
 exports.lambdaHandler = async (event, context) => {
     try {
         const body = JSON.parse(event.body);
+        await sendToDb(body);
+        console.log('Payload recorded');
         const device = findDevice(body.dev_id);
         if (device && device.thingspeak) {
             await sendToThingspeak(device, body.payload_fields);
             console.log('Device shown on Thingspeak #' + device.thingspeak.channel_id);
         }
-        const feed = {message: 'Hello world, ' + body.dev_id}; //await axios(url);
+        const feed = {message: 'Hello world, ' + body.dev_id};
         return {
             statusCode: 201,
             body: feed
@@ -53,7 +50,6 @@ exports.lambdaHandler = async (event, context) => {
         console.log(err);
         return err;
     }
-
 };
 
 function findDevice(devId) {
@@ -66,4 +62,17 @@ async function sendToThingspeak(device, payload) {
     payload.channel_id = device.thingspeak.channel_id;
     payload.api_key = device.thingspeak.api_key;
     return axios.post(url, payload);
+}
+
+async function sendToDb(payload) {
+    var AWS = require('aws-sdk');
+    AWS.config.update({region: region});
+    var docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+
+    payload.timestamp = payload.metadata.time;
+    var params = {
+        TableName: dbTable,
+        Item: payload
+    };
+    return docClient.put(params).promise();
 }
